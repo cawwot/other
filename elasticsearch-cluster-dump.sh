0f4024292b7a214403d6d1c78fb89bd9a4d7db81
Ian Spooner <cawwot@gmail.com>
2021-06-04 13:19:10 -0400

    Update elasticsearch-cluster-dump.sh

--- elasticsearch-cluster-dump.sh
+++ elasticsearch-cluster-dump.sh
@@ -3,6 +3,6 @@ DEST=$2
 indices=$(curl -s -XGET $INPUT/_cat/indices?h=i)
 for INDEX in $indices
 do
-  elasticdump --input=$INPUT/$INDEX --output=$DEST/$INDEX --type=mapping
-  elasticdump --input=$INPUT/$INDEX --output=$DEST/$INDEX --type=data
+  docker run --rm -ti elasticdump/elasticsearch-dump --input=$INPUT/$INDEX --output=$DEST/$INDEX --type=mapping
+  docker run --rm -ti elasticdump/elasticsearch-dump --input=$INPUT/$INDEX --output=$DEST/$INDEX --type=data
 done
